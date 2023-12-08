f3f81978ab0eea085e29740395e2b5cf1b32762d
Pritam Sharma <notpritamsharma@gmail.com>
2023-12-08 23:35:34 +0530

    Login Page Design

--- pocketh/app/login/page.js
+++ pocketh/app/login/page.js
@@ -1,11 +1,21 @@
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+
 function page() {
   return (
     <div className="min-w-screen flex items-center justify-center min-h-screen">
       <div className="max-w-[1440px] w-full flex gap-8">
-        <div>
-          <span className="text-[3rem] monster font-bold">
-            Let's get started<span className="text-secondary">.</span>
-          </span>
+        <div className="flex flex-col  gap-8">
+          <div className="flex flex-col gap-2">
+            <span className="text-[3rem] monster font-bold">
+              Let's get started<span className="text-secondary">.</span>
+            </span>
+            <span>Your gateway to a personalized experience.</span>
+          </div>
+
+          <Card className="p-8">
+            <Button className="w-full">Login</Button>
+          </Card>
         </div>
       </div>
     </div>
