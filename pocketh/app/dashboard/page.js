5904030cf272d9668caef3cdf8b10d91faec3ee7
Pritam Sharma <notpritamsharma@gmail.com>
2023-12-09 00:10:49 +0530

    Designs

--- pocketh/app/dashboard/page.js
+++ pocketh/app/dashboard/page.js
@@ -1,3 +1,4 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { FolderPlus, Trash2 } from "lucide-react";
@@ -13,7 +14,9 @@ function page() {
         <span className="text-[4rem] font-bold monster">0 ETH</span>
         <div className="flex flex-col items-end justify-start">
           <span>Available to Spend</span>
-          <Link href={"/"}>Get free funds</Link>
+          <Link className="text-blue-700" href={"/"}>
+            Get free funds
+          </Link>
         </div>
         <div className="flex gap-8">
           <Card className="p-4 flex cursor-pointer flex-col gap-4 items-center lg:max-w-[280px] opacity-50 hover:opacity-100 transition-all duration-300 ease-in-out">
@@ -38,6 +41,34 @@ function page() {
             Previous <span className="font-bold">pockEth</span>
           </span>
           <Link href={"/"}>View all</Link>
+        </div>
+
+        <div className="flex flex-col gap-4">
+          {Array.from({ length: 4 }).map((_, i) => (
+            <>
+              <Link href={`/pocket/${i}`}>
+                <Card className="p-2 gap-4 flex cursor-pointer justify-between pl-4 pr-4">
+                  <div className="flex gap-4">
+                    <Avatar>
+                      <AvatarImage src="https://github.com/shadcn.png" />
+                      <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+
+                    <div className="flex flex-col gap-2">
+                      <span className="font-bold">
+                        Name of the Pocket {"  " + i + 1}
+                      </span>
+                      <span className="text-[0.75rem]">Address :- 0xDeK7</span>
+                    </div>
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    <span className="font-bold">0.0001 ETH</span>
+                    <span className="text-[0.75rem]">0xDeK7</span>
+                  </div>
+                </Card>
+              </Link>
+            </>
+          ))}
         </div>
       </div>
     </div>
