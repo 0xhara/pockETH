41f5fde676d8d36bdfed7973c8dbc1cad36edfb7
srihar5ha <sri3har5ha@gmail.com>
2023-12-13 15:31:23 +0530

    light house storage

--- pocketh/app/page.js
+++ pocketh/app/page.js
@@ -2,6 +2,8 @@
 
 import { Button } from "@/components/ui/button";
 import { useEffect, useRef, useState } from "react";
+import lighthouse from "@lighthouse-web3/sdk";
+
 
 import { Badge } from "@/components/ui/badge";
 
@@ -327,6 +329,43 @@ export default function Home() {
     const txHash = await provider.waitForUserOperationTransaction(uo.hash);
 
     console.log(txHash, uo);
+  };
+
+   const progressCallback = (progressData) => {
+    let percentageDone =
+      100 - (progressData?.total / progressData?.uploaded)?.toFixed(2);
+    console.log(percentageDone);
+  };
+
+  const uploadFile = async (file) => {
+    // Push file to lighthouse node
+    // Both file and folder are supported by upload function
+    // Third parameter is for multiple files, if multiple files are to be uploaded at once make it true
+    // Fourth parameter is the deal parameters, default null
+
+    if(file == null){return null}    
+    
+    const output = await lighthouse.upload(
+      file,
+      process.env.NEXT_PUBLIC_LIGHTHOUSE_API,
+      false,
+      null,
+      progressCallback
+    );
+    console.log("File Status:", output);
+    /*
+      output:
+        data: {
+          Name: "filename.txt",
+          Size: 88000,
+          Hash: "QmWNmn2gr4ZihNPqaC5oTeePsHvFtkWNpjY3cD6Fd5am1w"
+        }
+      Note: Hash in response is CID.
+    */
+
+    console.log(
+      "Visit at https://gateway.lighthouse.storage/ipfs/" + output.data.Hash
+    );
   };
 
   const howItWorks = [
@@ -867,7 +906,10 @@ export default function Home() {
                         </div>
                       </div>
                       <DialogFooter>
-                        <Button onClick={sendTx} type="submit">
+                        <Button onClick={()=>{
+                          sendTx();
+                          // uploadFile(pictureRef.current.files);
+                        }} >
                           Create
                         </Button>
                       </DialogFooter>
@@ -913,7 +955,7 @@ export default function Home() {
                             sendFundsFromContract(
                               data[i],
                               "0x50c2ff55821401547F0319181aaBD916C9C1D026",
-                              "0.00001"
+                              "0.0001"
                             )
                           }
                         >
