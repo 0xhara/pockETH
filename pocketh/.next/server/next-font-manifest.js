e7346b9b05bf18784e2263b51c3fb910edde4ad3
srihar5ha <sri3har5ha@gmail.com>
2023-12-13 16:02:26 +0530

    restructure .next

--- pocketh/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/harsha/developer/ethindia/pockETH/pocketh/app/layout\":[]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
--- pocketh/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Ri2uV49jt7wGTTlOhYZ0el1VB2KgnfBPPG0p6NdlqT4=\"\n}"
